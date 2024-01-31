14bb02293946e5762eae0ce84fcd677351da9d8f
Eran Avraham <48490120+EranAvr@users.noreply.github.com>
2024-01-31 17:52:22 +0200

    Java if-else 1 uplaoded

---
+++ JavaBasics/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new  Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a * b = " + (a*b));
+        System.out.println("a / b = " + (a/b));
+        System.out.println("a . b = " + a + "." + b);
+    }
+}
