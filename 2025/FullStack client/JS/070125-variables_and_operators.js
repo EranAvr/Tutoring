16cbd5d6f305ac7d6f7181cda2c75cbf151e930b
Eran Avraham <48490120+EranAvr@users.noreply.github.com>
2025-01-07 19:48:50 +0200

    Add files via upload

---
+++ 2025/FullStack client/JS/070125-variables_and_operators.js
@@ -0,0 +1,42 @@
+console.log("check OK!");
+
+/*  Built-in Objects:    */
+console.log( console );
+console.log( window );
+console.log( window.innerHeight );
+
+console.log( document );
+
+
+/*  Variables:  */
+// variables and assignments:
+var num;
+var fName = "Bobby";
+var sentence = 'It is a beautiful day';
+
+console.log(num);
+console.log(fName);
+console.log(sentence);
+
+num = "10";
+sentence = 111;
+
+console.log(num);
+console.log(sentence);
+
+// variables and toString() :
+var bool = true;
+console.log(bool);
+console.log(bool.toString());
+var myNumber = 1001;
+console.log(myNumber);
+console.log(myNumber.toString());
+
+
+
+
+
+
+
+
+
