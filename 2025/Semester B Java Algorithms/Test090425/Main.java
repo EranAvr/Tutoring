567965fbd5e2c96b6c8091e554211253d2718d8a
Eran Avraham <48490120+EranAvr@users.noreply.github.com>
2025-04-10 17:39:59 +0300

    Add files via upload

---
+++ 2025/Semester B Java Algorithms/Test090425/Main.java
@@ -0,0 +1,17 @@
+package Test090425;
+
+public class Main {
+    public static void main(String[] args) {
+        // ex3a
+        /*
+            k   b[2*k]  b[2*k+1]    one     two     mid
+                                    0       5       3
+            0   7       -1          1       4
+            1   11      -1          2       3
+            2   11      -7          3       2
+            3   ------------------------------------
+         */
+        // ex3b
+        // a = [4,6,9,2,5,3]
+    }
+}
