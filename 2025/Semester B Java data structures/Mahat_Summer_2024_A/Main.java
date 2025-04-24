794f80452260d518679dc7971bc9e9f670615a5e
Eran Avraham <48490120+EranAvr@users.noreply.github.com>
2025-04-24 22:39:03 +0300

    Add files via upload

--- 2025/Semester B Java data structures/Mahat_Summer_2024_A/Main.java
+++ 2025/Semester B Java data structures/Mahat_Summer_2024_A/Main.java
@@ -1,9 +1,6 @@
 package Mahat_Summer_2024_A;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.Random;
-import java.util.Stack;
+import java.util.*;
 
 
 public class Main {
@@ -114,7 +111,71 @@ public class Main {
             לכן O(n^2)
          */
 
+        // ex10a
+        /*
+            1. "AA constructor"
+            2. "BB constructor"
+            3. "BB constructor"
+            4. "one in AA"
+            5. "one in BB"
+            6. "one in BB"
+            7. "one in AA"
+            8. "two in BB"
+            9. "two in BB"
+            10. "one in AA"
+            11. "one in AA"
+         */
+        /*
+            1) a.three();
+            הפקודה לא תקינה.
+            האובייקט a הוא מטיפוס-רפרנס AA ולכן לא מוגדרת לו הפונ' three() בממשק
+            שגיאת קומפילציה! הקומפיילר מבין שלא קיימת כזאת מתודה לטיפוס הנ"ל
 
+            2) ((BB)a).three();
+            הפקודה לא תקינה.
+            למרות שההמרה לטיפוס BB מתקבלת ע"י הקומפיילר ובדיקת הממשק של BB,
+            אבל לא קיימת שום מתודה three() עבור האובייקט a.
+            זאת מכיוון שאיתחלנו אותו עם הבנאי AA שבכלל לא בונה כזאת מתודה.
+         */
+
+        // ex10b
+        // המימוש למטה
+        AA a = new BB();
+        System.out.println(a instanceof BB);
+        System.out.println(a instanceof AA);
+        System.out.println(a instanceof Object);
+
+        // ex11
+        // המימוש למטה
+
+
+        // ex12
+        // a)
+        // Before: 10 -> 12 -> 5 -> 3 -> 6 -> 1 -> 4
+        // After: 10 -> 0 -> 12 -> 0 -> 5 -> 3 -> 6 -> 0 -> 1 -> 4 -> 0
+
+        // b)
+        /*
+            .הפעולה הרקורסיבית סורקת את כל החוליות המקוריות של הרשימה המקושרת
+            אחרי כל חוליה עם ערך זוגי, נוסיף חלויה חדשה עם הערך 0.
+         */
+
+        // c)
+        // Before: 10 -> 12 -> 5 -> 3 -> 6 -> 1 -> 4
+        // After: 10 -> 5 -> 3 -> 1
+
+        // d)
+        /*
+            הפעולה מוחקת מהשרשרת את כל החוליות עם ערך זוגי, חוץ (!!) מהחוליה הראשונה.
+            החוליה הראשונה נשארת לא נגועה, ולא נבדקת.
+         */
+
+        // e)
+        /*
+            הפעולה סורקת את השרשרת עד מציאת ערך אי-זוגי ראשון.
+            הפעולה תחזיר רפרנס אל אותה חוליה אבל לאחר מחיקת כל הזוגיים שאחריה.
+            אם לא נמצאו ערכים אי זוגיים כלל- הפעולה תחזיר null.
+         */
     }
 
     public static boolean isBalanced(Stack<Integer> st){
@@ -350,5 +411,137 @@ public class Main {
             size--;
         }
         return q3;
+    }
+
+    // ex10
+    public static void countMyTypes(Object[] arr){
+        int countBB = 0;
+        int countAAnotBB = 0;
+        int countNotAA = 0;
+        for (int i=0; i<arr.length; i++){
+            if (arr[i] instanceof BB)
+                countBB++;
+            else if ( arr[i] instanceof AA)
+                countAAnotBB++;
+            else
+                countNotAA++;
+        }
+        System.out.println("BB: " + countBB);
+        System.out.println("AA not BB: " + countAAnotBB);
+        System.out.println("Not AA: " + countNotAA);
+    }
+}
+
+class AA{}
+class BB extends AA{}
+
+class Student{
+    private String id;
+    private String name;
+    private String city;
+    private boolean driving;
+    private String lang;
+    private String secondLang;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public boolean isDriving() {
+        return driving;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public String getSecondLang() {
+        return secondLang;
+    }
+}
+
+class StudentsData{
+    private Node<Student> students;
+
+    public void eraseStudent(String id){
+        // מקרה קצה - מחיקת החוליה הראשונה
+        if (students.getValue().getId().equals(id))
+            students = students.getNext();
+
+        // שאר המקרים - מחיקת חוליה בהמשך השרשרת
+        //  Node(2) -> Node(5) -> Node(3) -> null;
+        //    |
+        //  temp
+        Node<Student> temp = students;
+        Node<Student> prev = null;
+        while (temp != null){
+            if (temp.getValue().getId().equals(id))
+                break;
+            prev = temp;
+            temp = temp.getNext();
+        }
+
+        if (temp != null)
+            prev.setNext(temp.getNext());
+    }
+
+    public void print(String city, String lang){
+        boolean flag = false;
+
+        Node<Student> temp = students;
+        while (temp != null){
+            Student s = temp.getValue();
+            if (s.getCity().equals(city) && (s.getLang().equals(lang) || s.getSecondLang().equals(lang))){
+                flag = true;
+                System.out.println(s.getName());
+            }
+            temp = temp.getNext();
+        }
+
+        if (!flag){
+            temp = students;
+            while (temp != null){
+                Student s = temp.getValue();
+                if (s.isDriving() && (s.getLang().equals(lang) || s.getSecondLang().equals(lang))){
+                    System.out.println(s.getName());
+                }
+            }
+        }
+    }
+
+    public int numStudents(String city){
+        int count = 0;
+        Node<Student> temp = students;
+
+        while (temp != null){
+            if (temp.getValue().getCity().equals(city))
+                count++;
+            temp = temp.getNext();
+        }
+
+        return count;
+    }
+
+    public String cityName(String[] cities){
+        String maxCity = cities[0];
+        int maxy = numStudents(maxCity);
+
+        for (int i=1; i<cities.length; i++){
+            int temp = numStudents(cities[i]);
+            if (temp > maxy){
+                maxy = temp;
+                maxCity = cities[i];
+            }
+        }
+
+        return maxCity;
     }
 }
