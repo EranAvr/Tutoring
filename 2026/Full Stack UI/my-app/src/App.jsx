70677e9ca0aa2107fbc8fe27e3e8661ca9f7d775
Eran Avraham <48490120+EranAvr@users.noreply.github.com>
2025-11-17 14:25:03 +0200

    Add files via upload

--- 2026/Full Stack UI/my-app/src/App.jsx
+++ 2026/Full Stack UI/my-app/src/App.jsx
@@ -3,14 +3,48 @@ import './App.css'
 import MyTitle from './components/my_title'
 import MyList from './components/my_list'
 import Button from './components/button'
+import { useState } from 'react'
+import UpdaterButton from './components/updater'
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  function increment(){
+    setCount(count + 1);
+  }
+
+  const [log, setLog] = useState("Off");
+  function logging(){
+    log === "Off" ? setLog("On") : setLog("Off");
+  }
+
   return (
     <>
-      <MyTitle/>
-      <MyList/>
-      
-      <Button />
+      {/*<MyTitle text="My Title Component" color={"red"}/>
+      <Button/>*/}
+      <nav>
+        User is: {log} <br />
+        <UpdaterButton text="logging" action={logging}/>
+      </nav>
+
+
+
+
+
+
+      <hr />
+
+
+
+
+
+
+
+
+      <div>
+        <h3>Your count: {count}</h3>
+        <UpdaterButton text="update" action={increment}/>
+      </div>
     </>
   )
 }
