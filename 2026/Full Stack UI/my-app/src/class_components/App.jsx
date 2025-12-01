d60b7ca2bbcb5d6e2f6265fba628b1780049af69
Eran Avraham <48490120+EranAvr@users.noreply.github.com>
2025-12-01 10:18:44 +0200

    Add files via upload

--- 2026/Full Stack UI/my-app/src/class_components/App.jsx
+++ 2026/Full Stack UI/my-app/src/class_components/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 import MyTitle from './class_components/my_title'
 import ColorTitle from './class_components/color_title'
-import MyContainer from './class_components/my_container'
+import SomeContainer from './class_components/my_container'
 import ClickedContainer from './class_components/clicked_container'
 
 function App() {
@@ -16,11 +16,11 @@ function App() {
 
 <hr />
 
-      <MyContainer>
+      <SomeContainer>
               <ColorTitle color="red" />
               <p>Some long text...</p>
               <img src="" alt="no photo" />
-      </MyContainer>
+      </SomeContainer>
 
 <hr />
 
