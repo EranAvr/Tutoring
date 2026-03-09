86d8b7f7f96c0e3ac0c8b5b2ddc5f3581c04305e
Eran Avraham <48490120+EranAvr@users.noreply.github.com>
2026-03-09 19:43:11 +0200

    Add files via upload

---
+++ 2026/Full Stack server/lesson090326/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header></header>
+    <nav></nav>
+    <aside></aside>
+    <main>
+        <h1> <?php echo "Company Portfolio"; ?> </h1>
+        <img src="<?php echo "https://img.freepik.com/free-photo/closeup-scarlet-macaw-from-side-view-scarlet-macaw-closeup-head_488145-3540.jpg?semt=ais_rp_50_assets&w=740&q=80"; ?>" alt="">
+        <h3></h3>
+        <p>
+            This is a portfolio page about the company <br>
+            Test page for php.
+        </p>
+    </main>
+    <footer></footer>
+</body>
+</html>
---
+++ 2026/Full Stack server/lesson090326/intro.php
@@ -0,0 +1,21 @@
+<?php /* Single line php tag */ ?>
+
+<?php 
+// Multi line php tag
+// Some more cope
+?>
+
+<?php
+// All of the document is php
+//phpinfo();
+
+// echo command/function-
+echo "Hello World!\n";
+echo("Hello World!\n");
+// print command/function-
+print "Hello World!\n";
+print("Hello World!\n");
+
+// print multiple values-
+// פחות יעיל ופחות בשימוש
+echo "A", "B", "C";
